ae4e55dc941219af67a4e58ee3409e17bf81f1c6
zqpzzj <1844066417@qq.com>
2020-07-08 22:33:23 +0800

    更改数据库

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -13,10 +13,10 @@ declare(strict_types=1);
 return [
     'default' => [
         'driver' => env('DB_DRIVER', 'mysql'),
-        'host' => env('DB_HOST', '49.235.134.38'),
+        'host' => env('DB_HOST', '8.129.176.243'),
         'port' => env('DB_PORT', 3306),
         'database' => env('DB_DATABASE', 'app'),
-        'username' => env('DB_USERNAME', 'zqp113'),
+        'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', 'Zqp113217!@'),
         'charset' => env('DB_CHARSET', 'utf8mb4'),
         'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
